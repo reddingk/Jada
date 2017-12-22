b7158a8822d90e2f8df6171b2d0995423303e7b1
Kris Redding <kredding@autoscribe.com>
2017-12-22 17:12:48 -0500

    update cells

--- jada_3/jcell.js
+++ jada_3/jcell.js
@@ -3,12 +3,22 @@
  * JADA CELL CLASS
  * By: Kris Redding
  */
+var request = require('request');
 
 const Tools = require('./jtools.js');
 
 class JCELL {  
     constructor() {
         this.jtools = new Tools();
+        this.apiLib = {
+            "tasteKid": {"link":"http://www.tastekid.com/api/","key":"228198-JadenPer-P426AN1R"},
+            "openWeather": {"link":"http://api.openweathermap.org/data/2.5/", "key":"90c2be179d4c18b392e3e11efa2ee5c1"},
+            "iTranslate4": {"link":"http://itranslate4.eu/api/", "key":"d803c195-58f2-4b3d-adcf-4e5bfcc368c2"},
+            "googleMapsDirections": {"link":"https://maps.googleapis.com/maps/api/directions/json", "key":"AIzaSyDmVwV-ugBBFPH9QxtFEPubd2X5ojRAH3o"},
+            "googlePlaces":{"link":"https://maps.googleapis.com/maps/api/place/autocomplete/json", "key":"AIzaSyBwnJZ2hoaIBKMGHMqEFEqF_faxUTBfcMs"},
+            "marvel": {"link":"https://gateway.marvel.com/", "key":"360f1fe1e9174b58521e32bb17e567fe", "privateKey":"2b05e2994b069d96faa718ad5ccf890aa13e100f"},
+            "geoAPI": {"link":"http://ip-api.com/json"}
+        };
     }
     
     getLocalDateTime(items, callback){
@@ -65,7 +75,72 @@ class JCELL {
         }
         callback(response);
     }
+
+    /* Media Compare using tastekid api */
+    mediaCompare(items, callback){
+        var self = this;
+        var response = {"error":null, "results":null};
+        var api = self.getApiItem("tasteKid");
+
+        if(api != null){
+            try {
+                if(!self.checkParameterList(["type", "query", "limit", "info"], items)){
+                    response.error = "Missing Parameter";
+                }
+                else {                    
+                    var url = self.jtools.stringFormat("{0}similar?q={1}&k={2}&type={3}&info={4}&limit={5}",[api.link, items.query.replace(" ", "+"), api.key, items.type, items.info, items.limit]);
+
+                    request({ url: url, json: true}, function (error, res, body){
+                        if(!error && res.statusCode === 200){
+                            response.results = body;                                
+                            callback(response);
+                        }
+                    });
+                }
+            }
+            catch(ex){
+                response.error = "Error Proccessing API Request: " + ex;                
+                callback(response);
+            }
+        }
+        else {
+            response.error = "Unable to retrieve API data";
+            callback(response);
+        }        
+    }  
     
+    /* Weather Info using open weather map API */
+    weatherInfo(items, callback){
+        var self = this;
+        var response = {"error":null, "results":null};
+        var api = self.getApiItem("openWeather");
+
+        if(api != null){
+            try {
+                if(!self.checkParameterList(["type", "location"], items)){
+                    response.error = "Missing Parameter";
+                }
+                else {                    
+                    var url = self.jtools.stringFormat("{0}{1}?q={2}&appid={3}&units=imperial", [api.link, items.type, items.location.replace(" ", "+"), api.key]);
+                                        
+                    request({ url: url, json: true}, function (error, res, body){
+                        if(!error && res.statusCode === 200){
+                            response.results = body;                                                   
+                            callback(response);
+                        }
+                    });
+                }
+            }
+            catch(ex){
+                response.error = "Error Proccessing API Request: " + ex;                
+                callback(response);
+            }
+        }
+        else {
+            response.error = "Unable to retrieve API data";
+            callback(response);
+        }
+    }
     
     /* private methods */
     checkParameterList(params, items){        
@@ -75,6 +150,12 @@ class JCELL {
             }
         }
         return true;
+    }
+
+    getApiItem(name){
+        var self = this;
+        var item = self.apiLib[name];
+        return (item == undefined ? null : item);
     }
 }
 
--- jada_3/jnerveSystem.js
+++ jada_3/jnerveSystem.js
@@ -80,18 +80,172 @@ class JNERVESYSTEM {
     /* Get Local Date*/
     getLocalDate(response, callback){
         var self = this;   
-        self.jcell.getLocalDateTime({"type":"date"},
-            function(res){
-                var finalResponse = null;
-                if(res.error == null){
-                    var timeRes = res.results;
-                    finalResponse = self.jtools.stringFormat("The date according to this machine is {0}", [timeRes]);
+        self.jcell.getLocalDateTime({"type":"date"}, function(res){
+            var finalResponse = null;
+            if(res.error == null){
+                var timeRes = res.results;
+                finalResponse = self.jtools.stringFormat("The date according to this machine is {0}", [timeRes]);
+            }
+            else {
+                finalResponse = "There was an error while retrieving the time, sorry check back later.";
+            }
+            callback({"jresponse": finalResponse});
+        });    
+    }
+
+    /* Get TasteKid Results */
+    getTastekidResults(response, callback){
+        var self = this;
+        var objectList = ["media","books","movies","music","shows","games","authors"];
+        var dataObj = {"type":"all", "query":"", "limit":10, "info":0};
+
+        // Parse phrase
+        for(var i=0; i < objectList.length; i++){
+            var objectPos = response.fullPhrase.indexOf(objectList[i]);
+            var simPos = response.fullPhrase.indexOf("similar to");
+            if((objectPos >= 0 && simPos > 0) && objectPos < simPos){
+                dataObj.query = response.fullPhrase.substring(simPos + 11);
+                dataObj.type = objectList[i];
+
+                // check for results limit in phrase
+                if(objectPos > 0){
+                    var preObject = response.fullPhrase.substring(0,objectPos).split(" ");
+                    for(var j=0; j < preObject.length ; j++){
+                        if(!isNaN(parseInt(preObject[j]))){
+                            dataObj.limit = parseInt(preObject[j]);
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        
+        // Call API
+        self.jcell.mediaCompare(dataObj, function(res){
+            var finalResponse = null;
+            if(res.error == null && res.results != null){
+                if(res.results.Similar.Results.length == 0){
+                    finalResponse = "Sorry there are no media suggestions for " + res.results.Similar.Info.map(function(el){ return el.Name; }).join(";") + ", maybe you have the wrong title(s)?";
                 }
                 else {
-                    finalResponse = "There was an error while retrieving the time, sorry check back later.";
+                    var inputString = res.results.Similar.Info.map(function(el){ return self.jtools.stringFormat("{0} ({1})", [el.Name, el.Type]); }).join(" & ");
+                    var resultsString = res.results.Similar.Results.map(function(el){ return self.jtools.stringFormat("{0} ({1})", [el.Name, el.Type]); }).join(", ");
+
+                    finalResponse = self.jtools.stringFormat("According to Tastekid for {0}. The following are sugguested that you checkout: {1}", [inputString, resultsString]);
+                }
+            }
+            else {
+                finalResponse = "There was an error while retrieving media compare data: " + res.error;
+            }
+            callback({"jresponse": finalResponse});
+        });
+    }
+
+    /* Get Current Weather */
+    getWeatherCurrent(response, callback){
+        var self = this;
+        var finalResponse = null;
+        var dataObj = {"type":"find"};
+
+        var tmpPhrase = response.fullPhrase.split(" ");
+        var postPhrase = tmpPhrase.slice(tmpPhrase.indexOf("weather"));
+        var forIndex = postPhrase.indexOf("for");
+
+        if(forIndex >= 0){
+            dataObj.location = postPhrase.slice(forIndex+1).join(" ");
+            
+            self.jcell.weatherInfo(dataObj, function(res){                 
+                if(res.error == null && res.results != null){
+                    if(res.results.count > 0) {
+                        finalResponse = self.jtools.stringFormat("The current weather accourding to OpenWeather.com for {0} is: Tempurature of {1}, Humidity of {2}%, with a description of '{3}'", [res.results.list[0].name, res.results.list[0].main.temp, res.results.list[0].main.humidity, res.results.list[0].weather[0].description ]);
+                    }
+                    else {
+                        finalResponse = self.jtools.stringFormat("Sorry we could not find the current weather for: {0}", [dataObj.location]);
+                    }
+                }
+                else {
+                    finalResponse = "There was an error while retrieving current weather data: " + res.error;
                 }
                 callback({"jresponse": finalResponse});
-        });    
+            });
+        }
+        else {
+            finalResponse = "Im not sure where you would like me to look";
+            callback({"jresponse": finalResponse});
+        }
+    }
+
+    /* Get Weather Forecast */
+    getWeatherForecast(response, callback){
+        var self = this;
+        var finalResponse = null;
+        var dataObj = {"type":"forecast"};
+
+        var tmpPhrase = response.fullPhrase.split(" ");
+        var postPhrase = tmpPhrase.slice(tmpPhrase.indexOf("forecast"));
+        var forIndex = postPhrase.indexOf("for");
+
+        if(forIndex >= 0){
+            dataObj.location = postPhrase.slice(forIndex+1).join(" ");
+            
+            self.jcell.weatherInfo(dataObj, function(res){                 
+                if(res.error == null && res.results != null){                   
+                    if(res.results.list.length > 0) {                        
+                        // Set initial parameters
+                        var dateString = (new Date(res.results.list[0].dt_txt)).toDateString();
+                        var dateNum = 0;
+                        var avgTemp = 0;
+                        var avgStatus = {}; 
+                        var dateList = [];                        
+
+                        for(var i =0; i < res.results.list.length; i++){
+                            var item = res.results.list[i];
+                            var newDate = (new Date(item.dt_txt)).toDateString();
+                            if(newDate != dateString ) {
+                                var dateStatus = {"name":null, "count": 0};     
+                                var lrgObj = Object.keys(avgStatus).reduce(function(a, b){ return avgStatus[a] > avgStatus[b] ? a : b });
+                                
+                                if(lrgObj != null){
+                                    dateStatus.name = lrgObj;
+                                    dateStatus.count = avgStatus[lrgObj];
+                                }
+                                dateList.push(self.jtools.stringFormat(" |{0} : {1} degrees and '{2}'", [dateString, (avgTemp / dateNum).toFixed(2), dateStatus.name]));
+
+                                //Reset tmp Values
+                                dateString = newDate;
+                                avgTemp = 0;                                
+                                dateNum = 0;
+                                avgStatus = {};
+                            }
+                            else{
+                                dateNum +=1.0;
+                                avgTemp += parseFloat(item.main.temp_max);
+                                if(item.weather[0].main in avgStatus){
+                                    avgStatus[item.weather[0].main] += 1;
+                                }
+                                else {
+                                    avgStatus[item.weather[0].main] = 1;
+                                }
+                            }
+                        } 
+                        
+                        finalResponse = self.jtools.stringFormat("The weather forecast for the next few days accourding to OpenWeather.com for {0}: \n {1}",[res.results.city.name, dateList.join("\n")]);
+                    }
+                    else {
+                        finalResponse = self.jtools.stringFormat("Sorry we could not find the weather forecast for: {0}", [dataObj.location]);
+                    }
+                }
+                else {
+                    finalResponse = "There was an error while retrieving current weather data: " + res.error;
+                }
+                callback({"jresponse": finalResponse});
+            });
+        }
+        else {
+            finalResponse = "Im not sure where you would like me to look";
+            callback({"jresponse": finalResponse});
+        }
     }
 }
 
