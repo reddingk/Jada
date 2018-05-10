416de12dce384f803f4929534093743f05a71f89
Redding <reddingk@leidos.com>
2018-05-10 11:34:08 -0400

    add directData clean up Repo

--- jada_3/jbrain.js
+++ jada_3/jbrain.js
@@ -8,44 +8,45 @@ const Language = require('./jlanguage.js');
 const Nerves = require('./jnerves.js');
 var phraseDB = require('./config/models/phrases');
 
-class JBRAIN {  
+class JBRAIN {
     constructor() {
-        this.settingFile = __dirname + '/config/settings.json';           
+        this.settingFile = __dirname + '/config/settings.json';
         this.jlanguage = new Language();
-        this.jNerves = new Nerves(this);           
+        this.jNerves = new Nerves(this);
+        this.jCells = this.jNerves.jnervesystem.jcell;
     }
 
     /* Functions */
-    test(phrase) { 
+    test(phrase) {
         console.log("You entered in " + phrase);
-        phraseDB.find({ 'type' : 'phrase' }, function(err, res){            
-            console.log(" [2]>");                            
+        phraseDB.find({ 'type' : 'phrase' }, function(err, res){
+            console.log(" [2]>");
         });
     };
 
     polly(phrase) { console.log("You entered in " + phrase);};
-    
+
     /* Jada conversation function */
-    convo(phrase, callback) { 
+    convo(phrase, callback) {
         var self = this;
 
-        var tmpStr = phrase.split(" ");  
+        var tmpStr = phrase.split(" ");
         var phraseLibrary = null;
         var fullPhraseLibrary = null;
-        
-        self.jlanguage.searchPhrase(tmpStr, function(res){            
-            // Check Full Phrases    
+
+        self.jlanguage.searchPhrase(tmpStr, function(res){
+            // Check Full Phrases
             if(self.jlanguage.fullPhraseLib == null){
-                self.jlanguage.getFullPhrases(function(fullres){ 
-                    self.jlanguage.fullPhraseLib = fullres; 
-                    
+                self.jlanguage.getFullPhrases(function(fullres){
+                    self.jlanguage.fullPhraseLib = fullres;
+
                     var response = self.jlanguage.getCall(phrase, res);
 
                     if(response != null){
                         phrase = ( response.response == "N/A" ? "" : phrase);
                         self.jNerves.dataResponse(response, phrase, function(res){ callback(res); });
                     }
-                });  
+                });
             }
             else {
                 var response = self.jlanguage.getCall(phrase, res, callback);
@@ -57,7 +58,20 @@ class JBRAIN {
         });
     }
 
-    directData(){ }
+    /* Direct Access to Functions */
+    directData(functionName, items) {
+      var self = this;
+      var response = {};
+      try {
+        self.jCells[functionName](items, function(res){
+            return res;
+        });
+      }
+      catch(ex){
+        response.error = "Error calling directData function: "+ ex;
+        return response;
+      }
+    }
 }
 
 
