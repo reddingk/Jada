1c32ca69731947bcaef4011333990c40cabc84f7
reddingk <kris.redding3@gmail.com>
2021-03-11 17:46:50 -0500

    update cors

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const http = require('http').Server(app);
-const io = (process.env.DEBUG == true ?
-      require('socket.io')(http, { cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] }})
-    : require('socket.io')(http));
+
+const io = require('socket.io')(http, { 
+    cors: { 
+        origin: "*", 
+        methods: ["GET", "POST"] 
+    }
+});
+
 // set ports
 var port = process.env.PORT || 1003;
 
